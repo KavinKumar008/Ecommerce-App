f16d5fee9927ba07d50fc06a89564550da64e9a1
KavinKumar008 <kavikumarsraja@gmail.com>
2025-05-21 12:09:59 +0530

    Updated some frontend components

--- frontend/src/paymentpage/Payment.jsx
+++ frontend/src/paymentpage/Payment.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Payment = () => {
-  return <div>Payment</div>;
+  return (
+    <div>
+      <h1>Payment page</h1>
+    </div>
+  );
 };
 
 export default Payment;
