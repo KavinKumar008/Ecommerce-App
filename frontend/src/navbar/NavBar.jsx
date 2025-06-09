8c7eaeaf1309b7e658d0770c4d7a42b447ae6083
KavinKumar008 <kavikumarsraja@gmail.com>
2025-06-09 14:58:34 +0530

    search fields updated

--- frontend/src/navbar/NavBar.jsx
+++ frontend/src/navbar/NavBar.jsx
@@ -1,49 +1,50 @@
 import React, { useState } from "react";
 import brandlogo from "../assets/brandlogo.jpeg";
-import { FaHome } from "react-icons/fa";
-import { FaShoppingCart } from "react-icons/fa";
+import { FaHome, FaShoppingCart, FaSearch } from "react-icons/fa";
 import { CgProfile } from "react-icons/cg";
-import { FaSearch } from "react-icons/fa";
-import { useNavigate } from "react-router-dom";
-// import { ElectronicItem } from "../productspage/ElectronicItem";
-import { useLocation, Link } from "react-router-dom";
+import { useNavigate, useLocation, Link } from "react-router-dom";
 import { useCart } from "../CartProvider";
 import { useProducts } from "../ProductProvider";
-import ProductCard from "../productcart/ProductCard";
 
 const NavBar = () => {
   const { cartItems } = useCart();
   const navigate = useNavigate();
   const { products, searchTerm, setSearchTerm } = useProducts();
   const location = useLocation();
-  console.log(location, "Locationnnnn");
-  console.log(cartItems, "cartitemssssnavbar");
-  console.log(products, "wdkwdwk", searchTerm);
-  function handleToHome() {
-    navigate("/products");
-  }
-
-  function handleProfilePage() {
-    navigate("/profile");
-  }
-
-  // function handleCartPage() {
-  //   navigate("/cartpage");
-  // }
-
-  const filteringProducts = products.filter((item) => {
-    item.name.toLowerCase().includes(searchTerm.toLocaleLowerCase());
-  });
-
-  console.log(filteringProducts, "filterrinefweifjiofj");
 
   const cartCount = cartItems.reduce(
     (accu, currentValue) => accu + (currentValue.quantity || 1),
     0
   );
-  console.log(cartCount, "cartcount");
+
+  const handleToHome = () => {
+    navigate("/products");
+  };
+
+  const handleProfilePage = () => {
+    navigate("/profile");
+  };
+
+  const handleSearchTerm = () => {
+    const foundItem = products.find(
+      (product) =>
+        product.name.toLowerCase() === searchTerm.trim().toLowerCase()
+    );
+
+    const category = foundItem?.category;
+    const id = foundItem?._id;
+
+    if (foundItem) {
+      navigate(`/productdetails/${category}/${id}`);
+      setSearchTerm("");
+    } else {
+      alert("Product Not Found");
+      setSearchTerm("");
+    }
+  };
+
   return (
-    <div className="bg-white fixed top-0 z-10 shadow-md p-2 w-full sm:w-full md:w-full lg:w-full">
+    <div className="bg-white fixed top-0 z-10 shadow-md p-2 w-full">
       <section className="flex justify-around items-center lg:pl-7 lg:pr-9">
         <div>
           <img
@@ -52,6 +53,7 @@ const NavBar = () => {
             onClick={handleToHome}
           />
         </div>
+
         <div className="relative lg:w-[400px] lg:block hidden">
           <FaSearch className="absolute top-1/2 left-3 transform -translate-y-1/2 text-gray-600" />
           <input
@@ -60,15 +62,44 @@ const NavBar = () => {
             className="pl-10 pr-3 py-3 xl:w-[500px] lg:w-[430px] outline-0 rounded-lg bg-[aliceblue]"
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                handleSearchTerm();
+              }
+            }}
           />
+
+          {searchTerm &&
+            products
+              .filter((item) =>
+                item.name.toLowerCase().startsWith(searchTerm.toLowerCase())
+              )
+              .slice(0, 5)
+              .map((product) => (
+                <div
+                  key={product._id}
+                  className="absolute bg-white w-full shadow-md border border-gray-200 z-20 p-2 hover:bg-gray-100 cursor-pointer rounded-md mt-3"
+                  onClick={() => {
+                    setSearchTerm("");
+                    navigate(
+                      `/productdetails/${product.category}/${product._id}`
+                    );
+                  }}
+                >
+                  <p className="flex items-center justify-center rounded-md">
+                    {product.name}
+                  </p>
+                </div>
+              ))}
         </div>
 
-        <div className="flex lg:gap-20 gap-10 md:flex-none">
+        <div className="flex lg:gap-20 gap-10">
           {location.pathname !== "/products" && (
             <Link to="/products">
               <FaHome className="text-2xl cursor-pointer" />
             </Link>
           )}
+
           {location.pathname !== "/cartpage" && (
             <Link to="/cartpage" className="relative">
               <FaShoppingCart className="text-2xl cursor-pointer" />
@@ -79,21 +110,13 @@ const NavBar = () => {
               )}
             </Link>
           )}
+
           <CgProfile
             className="text-2xl cursor-pointer"
             onClick={handleProfilePage}
           />
         </div>
       </section>
-      {/* <div className="flex flex-wrap gap-4">
-        {filteringProducts.length > 0 ? (
-          filteringProducts.map((product) => (
-            <ProductCard key={product._id} product={product} />
-          ))
-        ) : (
-          <p>No products found.</p>
-        )}
-      </div> */}
     </div>
   );
 };
