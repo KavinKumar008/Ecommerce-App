168a3ece9e6b67eb1de9f857d3fff5641915b698
KavinKumar008 <kavikumarsraja@gmail.com>
2025-06-29 21:14:38 +0530

    styling updated

--- frontend/src/addnewproduct/AddnewProduct.jsx
+++ frontend/src/addnewproduct/AddnewProduct.jsx
@@ -4,11 +4,15 @@ import cameraimg1 from "../assets/cameraimgviews/cameraimg1.jpeg";
 const AddnewProduct = () => {
   return (
     <main className="bg-gray-200 h-screen flex items-center justify-center">
-      <section className="bg-white w-[950px] h-[500px] rounded-xl shadow-md flex flex-col">
-        <div className="flex items-center justify-center h-[30%]">
-          <img src={cameraimg1} alt="" className="rounded-full p-5" />
+      <section className="bg-white w-[950px] h-[550px] rounded-xl shadow-md flex flex-col">
+        <div className="flex items-center justify-center h-[25%]">
+          <img src="" className="rounded-full bg-red-300 w-[100px] h-[100px]" />
+          <label htmlFor="">
+            <input type="file" />
+            <RiImageAddFill />
+          </label>
         </div>
-        <div className="flex h-[70%]">
+        <div className="flex h-[75%]">
           <section className="flex flex-col justify-evenly w-[50%] p-4">
             <label htmlFor="" className="flex gap-5 items-center">
               <span className="w-20 text-lg font-bold">Name</span>
@@ -44,26 +48,67 @@ const AddnewProduct = () => {
               />
             </label>
           </section>
-          <section className="flex flex-col justify-evenly w-[50%]">
-            <label htmlFor="" className="">
-              <span>Price</span>
-              <input type="text" placeholder="enter your price" />
+          <section className="flex flex-col justify-evenly w-[50%] p-4">
+            <label htmlFor="" className="flex gap-5 items-center">
+              <span className="w-20 font-bold text-lg">Price</span>
+              <input
+                type="text"
+                placeholder="enter your price"
+                className="flex-1 outline-none border border-gray-200 p-2 rounded-md"
+              />
             </label>
-            <label htmlFor="">
-              <span>Discount</span>
-              <input type="text" placeholder="enter your discount" />
+            <label htmlFor="" className="flex gap-5 items-center">
+              <span className="w-20 font-bold text-lg">Discount</span>
+              <input
+                type="text"
+                placeholder="enter your discount"
+                className="flex-1 outline-none border border-gray-200 p-2 rounded-md"
+              />
             </label>
-            <div>
-              <label htmlFor="">Select your category</label>
-              <select name="" id="">
+            <div className="flex items-center gap-5">
+              <label htmlFor="" className="w-20 font-bold text-lg">
+                Select your category
+              </label>
+              <select
+                name=""
+                id=""
+                className="flex-1 outline-none border border-gray-200 rounded-md p-3"
+              >
                 <option value="">Electronic</option>
                 <option value="">SmartPhone</option>
                 <option value="">Featured</option>
               </select>
             </div>
+            <div className="flex gap-15 items-center">
+              <span className="w-20 font-bold text-lg">ChangeImages</span>
+              <div className="flex-1 flex gap-10">
+                <img
+                  src=""
+                  alt=""
+                  className="rounded-full bg-gray-700 w-[50px] h-[50px]"
+                />
+                <img
+                  src=""
+                  alt=""
+                  className="rounded-full bg-gray-700 w-[50px] h-[50px]"
+                />
+                <img
+                  src=""
+                  alt=""
+                  className="rounded-full bg-gray-700 w-[50px] h-[50px]"
+                />
+              </div>
+            </div>
           </section>
         </div>
-        <button>Add</button>
+        <div className="flex items-center justify-end pr-10 p-2">
+          <button
+            type="button"
+            className="cursor-pointer bg-green-600 p-2 w-[100px] text-white text-lg font-semibold rounded-lg"
+          >
+            Add
+          </button>
+        </div>
       </section>
     </main>
   );
