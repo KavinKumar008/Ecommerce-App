d330a3c1ec1fe5d8af71e155c0bb878bcd10b76c
KavinKumar008 <kavikumarsraja@gmail.com>
2025-07-01 21:35:55 +0530

    addnewproduct functionality uodated

--- frontend/src/addnewproduct/AddnewProduct.jsx
+++ frontend/src/addnewproduct/AddnewProduct.jsx
@@ -2,6 +2,34 @@ import React from "react";
 import cameraimg1 from "../assets/cameraimgviews/cameraimg1.jpeg";
 
 const AddnewProduct = () => {
+  const [addInputs, setAddInputs] = useState({
+    name: "",
+    price: "",
+    originalPrice: "",
+    discount: "",
+    rating: "",
+    description: "",
+    category: "",
+    changeImages: [],
+    image: "",
+  });
+  const [image, setImage] = useState(null);
+  const [changeImages, setChangeImages] = useState([]);
+
+  console.log(addInputs, "addinputsssssss");
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    setImage(URL.createObjectURL(file));
+    setAddInputs({ ...addInputs, image: file });
+  };
+
+  const handleMultipleImagesChange = (e) => {
+    const file = Array.from(e.target.files);
+    const imageUrls = file.map((file) => URL.createObjectURL(file));
+    setChangeImages(imageUrls);
+    setAddInputs({ ...addInputs, changeImages: file });
+  };
   return (
     <main className="bg-gray-200 h-screen flex items-center justify-center">
       <section className="bg-white w-[950px] h-[550px] rounded-xl shadow-md flex flex-col">
@@ -18,8 +46,16 @@ const AddnewProduct = () => {
               <span className="w-20 text-lg font-bold">Name</span>
               <input
                 type="text"
+                name="name"
+                value={addInputs.name}
                 placeholder="enter product name"
-                className="flex-1 p-2 outline-none border border-gray-200 rounded-md"
+                className="lg:flex-1 flex-1 p-2 outline-none border border-gray-200 rounded-md"
+                onChange={(e) =>
+                  setAddInputs({
+                    ...addInputs,
+                    [e.target.name]: e.target.value,
+                  })
+                }
               />
             </label>
             <label htmlFor="" className="flex gap-5 items-center">
@@ -27,6 +63,8 @@ const AddnewProduct = () => {
 
               <input
                 type="text"
+                name="originalPrice"
+                value={addInputs.originalPrice}
                 placeholder="enter the original price"
                 className="flex-1 p-2 outline-none border border-gray-200 rounded-md"
               />
@@ -73,10 +111,17 @@ const AddnewProduct = () => {
                 name=""
                 id=""
                 className="flex-1 outline-none border border-gray-200 rounded-md p-3"
+                onChange={(e) =>
+                  setAddInputs({
+                    ...addInputs,
+                    [e.target.name]: e.target.value,
+                  })
+                }
               >
-                <option value="">Electronic</option>
-                <option value="">SmartPhone</option>
-                <option value="">Featured</option>
+                <option value="">Select Catrgory</option>
+                <option value={"electronic" || ""}>Electronic</option>
+                <option value={"smartphone" || ""}>SmartPhone</option>
+                <option value={"featured" || ""}>Featured</option>
               </select>
             </div>
             <div className="xl:flex xl:gap-20 justify-evenly items-center flex gap-10">
@@ -97,6 +142,43 @@ const AddnewProduct = () => {
                   alt=""
                   className="rounded-full border w-[50px] h-[50px]"
                 />
+              </div>
+            </div> */}
+            <div className="flex items-center gap-10">
+              <label htmlFor="changeImagesInput">
+                <span className="w-20 font-bold lg:text-lg">ChangeImages</span>
+                <input
+                  type="file"
+                  multiple
+                  id="changeImagesInput"
+                  className="hidden"
+                  onChange={handleMultipleImagesChange}
+                />
+                {changeImages?.length === 0 ? (
+                  <RiImageAddFill className="cursor-pointer text-lg font-bold text-center" />
+                ) : (
+                  []
+                )}
+              </label>
+              <div className="w-[100%] flex justify-around">
+                {changeImages.map((image, ind) => (
+                  <div key={ind}>
+                    <img
+                      src={image}
+                      alt="changeimages"
+                      className="rounded-full border w-[50px] h-[50px]"
+                    />
+                  </div>
+                ))}
+                <p>
+                  {changeImages?.length === 0 ? (
+                    <span className="text-sm font-bold text-red-400">
+                      Select Maximum Three Images to Upload
+                    </span>
+                  ) : (
+                    []
+                  )}
+                </p>
               </div>
             </div>
           </section>
